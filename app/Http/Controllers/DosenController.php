a6d6fe6a039e122d31c8a0eef200fbb15d69f33c
AjrinNR <ajrinnr11@gmail.com>
2020-02-26 11:37:55 +0700

    CRUD Controllers Next Level

--- app/Http/Controllers/DosenController.php
+++ app/Http/Controllers/DosenController.php
@@ -7,89 +7,63 @@ use Illuminate\Http\Request;
 
 class DosenController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $dosen = Dosen::all();
-        return view ('dosen.index',compact('dosen'));
+        return view('dosen.index', compact('dosen'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
         return view('dosen.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
         $dosen = new Dosen();
         $dosen->nama = $request->nama;
         $dosen->nipd = $request->nipd;
         $dosen->save();
-        return redirect()->route('dosen.index')
-                ->with(['message'=>'Dosen Berhasil dibuat']);
+        return redirect()->route('dosen.index')->with(['message' => 'Dosen Berhasil Dibuat']);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Dosen  $dosen
-     * @return \Illuminate\Http\Response
-     */
+
     public function show($id)
     {
-        $dosen= Dosen::findOrFail($id);
-        return view ('dosen.show',compact('dosen'));
+        $dosen = Dosen::findOrFail($id);
+        return view('dosen.show', compact('dosen'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Dosen  $dosen
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit($id)
     {
         $dosen = Dosen::findOrFail($id);
-        return view('dosen.edit',compact('dosen'));
+        return view('dosen.edit', compact('dosen'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Dosen  $dosen
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, $id)
     {
         $dosen = Dosen::findOrFail($id);
         $dosen->nama = $request->nama;
         $dosen->nipd = $request->nipd;
         $dosen->save();
-        return redirect()->route('dosen.index')
-            -> with(['message'=>'Dosen Berhasil di buat']);
+        return redirect()->route('dosen.index')->with(['message' => 'Dosen Berhasil Diedit']);
     }
 
 
     public function destroy($id)
     {
         $dosen = Dosen::findOrFail($id)->delete();
-        return redirect()->route('dosen.index')
-            -> with(['message'=>'Dosen Berhasil di hapus']);
+
+        return redirect()->route('dosen.index')->with(['message' => 'Dosen Berhasil Dihapus']);
     }
+
 }
